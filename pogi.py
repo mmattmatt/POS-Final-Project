3ed583a3f7ab6e8233715f6ef4cd4762989fca18
mmattmatt <146624380+mmattmatt@users.noreply.github.com>
2024-11-11 17:45:23 +0800

    Delete pogi.py

--- pogi.py
+++
@@ -1,7 +0,0 @@
-while True:
-    try:
-        tanong = input(str("POGI KA BA? (yes or no) "))
-        if tanong.lower() != "yes":
-            print ("hmmmm")
-    except ValueError:
-        print ("INVALID")
