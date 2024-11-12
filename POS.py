fbb00a91eed0a6c7c6e31aa907456f48f172a24c
mmattmatt <146624380+mmattmatt@users.noreply.github.com>
2024-11-12 20:02:15 +0800

    Add files via upload
    Upgraded Version

--- POS.py
+++ POS.py
@@ -1,27 +1,59 @@
 foods = []
+quantities = []
 prices = []
 total = 0
 
+print("Menu:")
+print("Hamburger - Php.50")
+print("Chicken - Php.100")
+print()
+
+#Get order
+validItems = ["hamburger", "chicken"]
 while True:
-    print("\nMenu:")
-    print ()
     food = input("Enter a food to buy ('done' to quit): ")
     if food.lower() == 'done':
         break
+    elif food.lower() not in validItems:
+        print("Invalid menu item. Please choose from the following:")
+        for item in validItems:
+            print(f"- {item.capitalize()}")
     else:
+        quantity = int(input("Quantity: "))
         price = float(input(f"Enter the price of a {food}: Php."))
+        
         foods.append(food)
-        prices.append(price)
-print()
-print("______________________")
-print()
+        quantities.append(quantity)
+        prices.append(price * quantity)
+
+#Cart display
+print("________________________")
 print("YOUR CART:")
-for food in foods:
-    print(food)
-print("______________________")
+for i in range(len(foods)):
+    print(f"{quantities[i]} x {foods[i]} - Php.{prices[i]}")
+print("------------------------")
 
 for price in prices:
     total += price
-print()
 print(f"Your total is: Php.{total}")
-print("______________________")
+print("------------------------")
+
+#Payment
+while True:
+    cash_input = input("Enter your cash: ")
+    print (f"You've entered Php.{cash_input}")
+    print("________________________")
+    try:
+        cash = float(cash_input)
+        if cash < total:
+            print("Insufficient funds. Please try again.")
+        elif cash > total:
+            change = cash - total
+            print(f"Your change is Php.{change}")
+            break
+        else:
+            print("Exact amount. Thank you!")
+            break
+    except ValueError:
+        print("Invalid input. Please enter a number.")
+print("________________________")
